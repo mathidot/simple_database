cc216b23217f85c7b3274388a3e1f4d138495e53
os <c1216440698@126.com>
2022-04-13 00:56:25 -0700

    chapter 13

--- test.rb
+++ test.rb
@@ -1,5 +1,5 @@
 describe 'database' do
-  before do
+  before(:each)do
     'rm -rf test.db'
   end
 
@@ -7,7 +7,11 @@ describe 'database' do
     raw_output = nil
     IO.popen("./db test.db", "r+") do |pipe|
       commands.each do |command|
-        pipe.puts command
+        begin
+          pipe.puts command
+        rescue Error::EPIPE
+          break
+        end
       end
 
       pipe.close_write
@@ -58,45 +62,90 @@ describe 'database' do
       "db > Constants:",
       "ROW_SIZE: 293",
       "COMMON_NODE_HEAD_SIZE: 6",
-      "LEAF_NODE_HEADER_SIZE: 10",
+      "LEAF_NODE_HEADER_SIZE: 14",
       "LEAF_NODE_CELL_SIZE: 297",
-      "LEAF_NODE_SPACE_FOR_CELLS: 4086",
+      "LEAF_NODE_SPACE_FOR_CELLS: 4082",
       "LEAF_NODE_MAX_CELLS: 13",
       "db > ",
-    ])
-  end
-
-  it 'allows printing out the structure of a one-node btree' do
-    script = [3,1,2].map do |i|
-      "insert #{i} user#{i} person#{i}@email.com"
-    end
-    script << ".btree"
-    script << ".exit"
-    result = run_script(script)
-
-    expect(result).to match_array([
-        "db > Executed.",
-        "db > Executed.",
-        "db > Executed.",
-        "db > Tree:",
-        "leaf (size 3)",
-        "  - 0 : 1",
-        "  - 1 : 2",
-        "  - 2 : 3",
-        "db > ",
     ])
   end
 
   it 'prints an error message if there is a duplicate id' do
     script  = [
       "insert 1 user1 person1@email.com",
+      "insert 1 user2 person2@email.com",
       ".exit",
     ]    
     result = run_script(script)
     expect(result).to match_array([
+      "db > Executed.",
       "db > Error: Duplicate key.",
       "db > ",
     ])
   end
-  
+
+it 'allows printing out the structure of a 3-leaf-node btree' do
+    script = (1..14).map do |i|
+      "insert #{i} user#{i} person#{i}@example.com"
+    end
+    script << ".btree"
+    script << "insert 15 user15 person15@example.com"
+    script << ".exit"
+    result = run_script(script)
+
+    expect(result[14...(result.length)]).to match_array([
+      "db > Tree:",
+      "- internal (size 1)",
+      "  - leaf (size 7)",
+      "    - 1",
+      "    - 2",
+      "    - 3",
+      "    - 4",
+      "    - 5",
+      "    - 6",
+      "    - 7",
+      "  - key 7",
+      "  - leaf (size 7)",
+      "    - 8",
+      "    - 9",
+      "    - 10",
+      "    - 11",
+      "    - 12",
+      "    - 13",
+      "    - 14",
+      "db > Executed.",
+      "db > ",
+    ])
+  end
+
+  it 'prints all rows in a multi-level tree' do
+    script = []
+    (1..15).each do |i|
+      script << "insert #{i} user#{i} person#{i}@email.com"
+    end
+    script << "select"
+    script << ".exit"
+    result = run_script(script)
+
+    expect(result[15...result.length]).to match_array([
+      "db > (1, user1, person1@email.com)",
+      "(2, user2, person2@email.com)",
+      "(3, user3, person3@email.com)",
+      "(4, user4, person4@email.com)",
+      "(5, user5, person5@email.com)",
+      "(6, user6, person6@email.com)",
+      "(7, user7, person7@email.com)",
+      "(8, user8, person8@email.com)",
+      "(9, user9, person9@email.com)",
+      "(10, user10, person10@email.com)",
+      "(11, user11, person11@email.com)",
+      "(12, user12, person12@email.com)",
+      "(13, user13, person13@email.com)",
+      "(14, user14, person14@email.com)",
+      "(15, user15, person15@email.com)",
+      "Executed.",
+      "db > ",
+    ])    
+  end
+
 end
